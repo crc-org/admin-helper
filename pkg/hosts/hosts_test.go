1c20c5d74a4434bf1a4e21aaddce8ffa2dfbb40a
Christophe Fergeau <cfergeau@redhat.com>
2022-12-12 12:19:57 +0100

    Remove use of ioutil
    It's been deprecated since golang 1.16, and has replacements in the io or
    os packages.

--- pkg/hosts/hosts_test.go
+++ pkg/hosts/hosts_test.go
@@ -1,7 +1,6 @@
 package hosts
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -12,64 +11,64 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	dir, err := ioutil.TempDir("", "hosts")
+	dir, err := os.MkdirTemp("", "hosts")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
 
 	hostsFile := filepath.Join(dir, "hosts")
-	assert.NoError(t, ioutil.WriteFile(hostsFile, []byte(`127.0.0.1 entry1`), 0600))
+	assert.NoError(t, os.WriteFile(hostsFile, []byte(`127.0.0.1 entry1`), 0600))
 
 	host := hosts(t, hostsFile)
 
 	assert.NoError(t, host.Add("127.0.0.1", []string{"entry1", "entry2", "entry3"}))
 	assert.NoError(t, host.Add("127.0.0.2", []string{"entry4"}))
 
-	content, err := ioutil.ReadFile(hostsFile)
+	content, err := os.ReadFile(hostsFile)
 	assert.NoError(t, err)
 	assert.Equal(t, "127.0.0.1 entry1 entry2 entry3"+eol()+"127.0.0.2 entry4"+eol(), string(content))
 }
 
 func TestRemove(t *testing.T) {
-	dir, err := ioutil.TempDir("", "hosts")
+	dir, err := os.MkdirTemp("", "hosts")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
 
 	hostsFile := filepath.Join(dir, "hosts")
-	assert.NoError(t, ioutil.WriteFile(hostsFile, []byte(`127.0.0.1 entry1 entry2`), 0600))
+	assert.NoError(t, os.WriteFile(hostsFile, []byte(`127.0.0.1 entry1 entry2`), 0600))
 
 	host := hosts(t, hostsFile)
 
 	assert.NoError(t, host.Remove([]string{"entry2"}))
 
-	content, err := ioutil.ReadFile(hostsFile)
+	content, err := os.ReadFile(hostsFile)
 	assert.NoError(t, err)
 	assert.Equal(t, "127.0.0.1 entry1"+eol(), string(content))
 }
 
 func TestClean(t *testing.T) {
-	dir, err := ioutil.TempDir("", "hosts")
+	dir, err := os.MkdirTemp("", "hosts")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
 
 	hostsFile := filepath.Join(dir, "hosts")
-	assert.NoError(t, ioutil.WriteFile(hostsFile, []byte(`127.0.0.1 entry1.suffix1 entry2.suffix2`), 0600))
+	assert.NoError(t, os.WriteFile(hostsFile, []byte(`127.0.0.1 entry1.suffix1 entry2.suffix2`), 0600))
 
 	host := hosts(t, hostsFile)
 
 	assert.NoError(t, host.Clean([]string{".suffix1"}))
 
-	content, err := ioutil.ReadFile(hostsFile)
+	content, err := os.ReadFile(hostsFile)
 	assert.NoError(t, err)
 	assert.Equal(t, "127.0.0.1 entry2.suffix2"+eol(), string(content))
 }
 
 func TestContains(t *testing.T) {
-	dir, err := ioutil.TempDir("", "hosts")
+	dir, err := os.MkdirTemp("", "hosts")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
 
 	hostsFile := filepath.Join(dir, "hosts")
-	assert.NoError(t, ioutil.WriteFile(hostsFile, []byte(`127.0.0.1 entry1.suffix1 entry2.suffix2`), 0600))
+	assert.NoError(t, os.WriteFile(hostsFile, []byte(`127.0.0.1 entry1.suffix1 entry2.suffix2`), 0600))
 
 	host := hosts(t, hostsFile)
 
@@ -79,12 +78,12 @@ func TestContains(t *testing.T) {
 }
 
 func TestSuffixFilter(t *testing.T) {
-	dir, err := ioutil.TempDir("", "hosts")
+	dir, err := os.MkdirTemp("", "hosts")
 	assert.NoError(t, err)
 	defer os.RemoveAll(dir)
 
 	hostsFile := filepath.Join(dir, "hosts")
-	assert.NoError(t, ioutil.WriteFile(hostsFile, []byte(`127.0.0.1 localhost localhost.localdomain`), 0600))
+	assert.NoError(t, os.WriteFile(hostsFile, []byte(`127.0.0.1 localhost localhost.localdomain`), 0600))
 
 	file, err := hostsfile.NewCustomHosts(hostsFile)
 	assert.NoError(t, err)
